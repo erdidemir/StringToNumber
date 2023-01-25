8ba734fa148e783383414e3369dc47dd12e98c7c
ErdiDemir <erdi.demir@icloud.com>
2023-01-25 17:33:39 +0300

    testing none prural number

--- StringToText.UnitTest/StringToNumberUnitTest.cs
+++ StringToText.UnitTest/StringToNumberUnitTest.cs
@@ -6,13 +6,41 @@ namespace StringToText.UnitTest
     public class StringToNumberUnitTest
     {
         [Fact]
-        public void WhenGivingTwentyMillion_20000000_ShouldBeReturn()
+        public void WhenGivingTwentyMillions_20000000_ShouldBeReturn()
         {
             TextToNumberService textToNumberService = new TextToNumberService();
 
 
             string input = "He paid twenty millions for three such cars.";
             string output = "He paid 20000000 for 3 such cars.";
+
+            var result = textToNumberService.ConvertTextToNumber(input);
+
+            Assert.Equal(output, result);
+        }
+
+        [Fact]
+        public void WhenGivingFifityMillions_50000000_ShouldBeReturn()
+        {
+            TextToNumberService textToNumberService = new TextToNumberService();
+
+
+            string input = "He paid fifty millions for five such cars.";
+            string output = "He paid 50000000 for 5 such cars.";
+
+            var result = textToNumberService.ConvertTextToNumber(input);
+
+            Assert.Equal(output, result);
+        }
+
+        [Fact]
+        public void WhenGivingOneMillion_10000000_ShouldBeReturn()
+        {
+            TextToNumberService textToNumberService = new TextToNumberService();
+
+
+            string input = "He paid one million for one such cars.";
+            string output = "He paid 1000000 for 1 such cars.";
 
             var result = textToNumberService.ConvertTextToNumber(input);
 
